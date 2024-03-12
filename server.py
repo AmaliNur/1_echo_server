dba3738860ee85fc814189bb6912c6125b58981d
AmaliNur <amalnuritdinov5@gmail.com>
2024-03-12 17:37:44 +0300

    12.03.2024

--- server.py
+++ server.py
@@ -2,18 +2,22 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
+print("Server starting!")
 sock.listen(0)
+print("Port is listing")
 conn, addr = sock.accept()
-print(addr)
+print("Client accepted")
+print("Client adress: ", addr[0])
+print("Client port:", addr[1])
 
 msg = ''
 
 while True:
 	data = conn.recv(1024)
 	if not data:
-		break
+	break
 	msg += data.decode()
-	conn.send(data)
+	conn.send(msg.upper().encode())
 
 print(msg)
 
